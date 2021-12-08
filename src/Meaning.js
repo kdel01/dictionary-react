7ed0316a79ad36476bc109db9c1121a5b5dc284c
kdel01 <delaverek@gmail.com>
2021-12-08 16:51:32 +1100

    added synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
@@ -8,9 +9,13 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
+              <strong>Definition: </strong>
               {def.definition}
               <br />
+              <strong>Example: </strong>
               <em>{def.example}</em>
+              <br />
+              <Synonyms synonyms={def.synonyms} />
             </p>
             <hr />
           </div>
